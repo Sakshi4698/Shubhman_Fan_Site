518077e0e72b5bcbf57ec4be873f396b55c6e05d
Sakshi4698 <sakshiladda46@gmail.com>
2026-03-16 14:53:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Shubhman_Fan_Site/"
+  base: "/ShubhmanSite/"
 })
